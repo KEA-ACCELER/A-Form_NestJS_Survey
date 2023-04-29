8f6c9c41a3e6de2119b7f7e127ccc1d6adf9b814
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-04-29 23:16:46 +0900

    chore: mongoose setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from '@/app.controller';
 import { AppService } from '@/app.service';
 import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: () => ({
+        uri: `${process.env.DATABASE_URL}`,
+      }),
     }),
   ],
   controllers: [AppController],
