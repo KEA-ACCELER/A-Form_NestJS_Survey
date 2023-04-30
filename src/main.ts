673ae4f1c0789976e6d98076ea833a69e82441aa
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-04-30 09:35:47 +0900

    feat: add hot reload

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
 import { ConfigService } from '@nestjs/config';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const configService = app.get(ConfigService);
   await app.listen(configService.get('PORT') || 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
