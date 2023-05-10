9ebb646d5fdd220c44c1085a264a03800079c5a6
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-05-10 12:08:05 +0900

    refactor: add abSurvey answer validation

--- src/answer/helper/survey-check.helper.ts
+++ src/answer/helper/survey-check.helper.ts
@@ -1,7 +1,6 @@
-import { ABSurvey } from '@/common/enum';
 import { SurveyType } from '@/common/enum';
 import { SurveyService } from '@/survey/service/survey.service';
-import { Injectable, BadRequestException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Types } from 'mongoose';
 
 @Injectable()
@@ -11,14 +10,5 @@ export class SurveyCheckHelper {
   getSurveyType = async (_id: Types.ObjectId): Promise<SurveyType> => {
     const survey = await this.surveyService.findOne(_id);
     return survey.type;
-  };
-
-  checkABUpdate = (answers: any[]): ABSurvey => {
-    if (JSON.stringify(answers) === JSON.stringify(['A'])) {
-      return ABSurvey.A;
-    } else if (JSON.stringify(answers) === JSON.stringify(['B'])) {
-      return ABSurvey.B;
-    }
-    throw new BadRequestException();
   };
 }
