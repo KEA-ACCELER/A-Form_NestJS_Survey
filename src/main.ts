73284561742a29a19664f957eea79616f862ff5b
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-05-12 17:30:32 +0900

    refactor: add globalPrefix

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
 
   app.useGlobalPipes(
     new ValidationPipe({
