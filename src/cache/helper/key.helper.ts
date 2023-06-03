054a9483583e354e7a96dec6644a4066fe1e6098
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-06-04 07:21:15 +0900

    refactor: popular survey key timezone
    Asia.Seoul -> UTC

--- src/cache/helper/key.helper.ts
+++ src/cache/helper/key.helper.ts
@@ -1,12 +1,13 @@
-import moment from 'moment-timezone';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class KeyHelper {
   getPopularSurveyKey(date: Date): string {
-    // TODO: UTC or Asia/Seoul로 사용할건지
-    return `popular_survey:${moment(date)
-      .tz('Asia/Seoul')
-      .format('YYYYMMDDHH')}`;
+    const year = date.getFullYear().toString().slice(-2);
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    const hours = String(date.getHours()).padStart(2, '0');
+
+    return `popular_survey:${year}${month}${day}${hours}`;
   }
 }
