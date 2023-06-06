cd46ba1f31395a20628370313c5d65ded033f681
Yejin Choi <63734765+chldppwls12@users.noreply.github.com>
2023-06-06 23:19:54 +0900

    fix: add seHelper endpoint (#29)

--- src/file/helper/s3.helper.ts
+++ src/file/helper/s3.helper.ts
@@ -5,15 +5,20 @@ import { ManagedUpload } from 'aws-sdk/clients/s3';
 @Injectable()
 export class S3Helper {
   private readonly s3;
+  private endpoint = new AWS.Endpoint(process.env.AWS_ENDPOINT || '');
+  private region = process.env.REGION;
+  private accessKey = process.env.AWS_ACCESS_KEY_ID || '';
+  private secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || '';
 
   constructor() {
-    AWS.config.update({
+    this.s3 = new AWS.S3({
+      endpoint: this.endpoint,
+      region: this.region,
       credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
+        accessKeyId: this.accessKey,
+        secretAccessKey: this.secretAccessKey,
       },
     });
-    this.s3 = new AWS.S3();
   }
 
   async uploadFile(
@@ -22,9 +27,9 @@ export class S3Helper {
   ): Promise<ManagedUpload.SendData> {
     return await this.s3
       .upload({
+        Bucket: process.env.S3_BUCKET_NAME || '',
         Key: fileName,
         Body: file.buffer,
-        Bucket: process.env.S3_BUCKET || '',
       })
       .promise();
   }
